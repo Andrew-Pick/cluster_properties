00c913b7860a68e9a99def2bf3778e7253b25625
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-01 14:59:53 +0100

    Ran SimTesting for all six models

--- run_SimulationTesting.py
+++ run_SimulationTesting.py
@@ -2,13 +2,13 @@ from cluster_properties import SimulationTesting
 
 sim_test = SimulationTesting(
     simulation="L302_N1136",
-    models=["GR"],
-    realisations=["1"],
+    models=["GR","F40","F45","F50","F55","F60"],
+    realisations=["1","1","1","1","1","1"],
     snapshot=12,
     file_ending="all",
-    labels=["GR"],
-    colors=["black"],
+    labels=["GR","F40","F45","F50","F55","F60"],
+    colors=["black","blue","pink","green","yellow","red"],
     defaults=["Default config"],
-    plot_name="cluster_comparison"
+    plot_name="cluster_comparison_allModels"
 )
 sim_test.tng_observables()   # generates summary comparison plots
