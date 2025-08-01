282aeedf82e0f2ec2d6cb3d9a961b21dc093cb52
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-01 15:16:51 +0100

    Corrected arguments for running SimTesting

--- run_SimulationTesting.py
+++ run_SimulationTesting.py
@@ -2,13 +2,14 @@ from cluster_properties import SimulationTesting
 
 sim_test = SimulationTesting(
     simulation="L302_N1136",
-    models=["GR","F40","F45","F50","F55","F60"],
+    models=["GR","F60","F55","F50","F45","F40"],
     realisations=["1","1","1","1","1","1"],
     snapshot=12,
     file_ending="all",
-    labels=["GR","F40","F45","F50","F55","F60"],
-    colors=["black","blue","pink","green","yellow","red"],
-    defaults=["Default config"],
+    labels=["L302-N1136-GR", "L302-N1136-F60", "L302-N1136-F55",
+            "L302-N1136-F50", "L302-N1136-F45", "L302-N1136-F40"],
+    colors=["black","blue","magenta","green","orange","red"],
+    defaults=[],
     plot_name="cluster_comparison_allModels"
 )
 sim_test.tng_observables()   # generates summary comparison plots
