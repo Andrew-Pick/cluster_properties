4e64fcb49a1894cbc6984439a8533785a507a843
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-03 09:16:34 +0100

    Changed to just GR due to error

--- run_SimulationTesting.py
+++ run_SimulationTesting.py
@@ -2,14 +2,14 @@ from cluster_properties import SimulationTesting
 
 sim_test = SimulationTesting(
     simulation="L302_N1136",
-    models=["GR","F40"],
-    realisations=["1","1"],
+    models=["GR"],
+    realisations=["1"],
     snapshot=12,
     file_ending="all",
-    labels=["L302-N1136-GR", "L302-N1136-F40"],
-    colors=["black","red"],
+    labels=["L302-N1136-GR"],
+    colors=["black"],
     defaults=[],
-    plot_name="test_MyData_F40_GR",
+    plot_name="test_MyData_GR",
     show_spread=True
 )
 sim_test.tng_observables()   # generates summary comparison plots
