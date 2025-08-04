2cd0305cd0c2f5fabfca951f104b2b20b73b552c
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-04 13:37:59 +0100

    changed model to F50

--- run_ClusterProperties.py
+++ run_ClusterProperties.py
@@ -2,7 +2,7 @@ from cluster_properties import ClusterProperties
 
 cp = ClusterProperties(
     simulation="L302_N1136",
-    model="F45",
+    model="F50",
     realisation="1",
     snapshot=12,
     mass_cut=1e11,
