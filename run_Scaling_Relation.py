29c79d4447f06055de609be5e26ab6d89322d36e
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-04 16:21:47 +0100

    Run scaling relation for GR

--- run_Scaling_Relation.py
+++ run_Scaling_Relation.py
@@ -1,6 +1,17 @@
 from scaling_relations import Scaling_Relation
 
 sr = Scaling_Relation(
+    simulation="L302_N1136",
+    models=["GR"],
+    realisations=["1"],
+    snapshot=12,
+    file_ending="all",
+    labels=["L302-N1136-GR"],
+    colors=["black"],
+    defaults=[],
+    plot_name="scaling_relation_GR",
+    show_spread=False
+)
 
 
-sr.
+sr.redshift()
