7514149fbbe5d6ec53805bf51a80b2651e6f8d49
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-04 16:31:51 +0100

    Run Scaling Relation for all models

--- run_Scaling_Relation.py
+++ run_Scaling_Relation.py
@@ -2,14 +2,14 @@ from scaling_relations import Scaling_Relation
 
 sr = Scaling_Relation(
     simulation="L302_N1136",
-    models=["GR"],
-    realisations=["1"],
+    models=["GR","F60","F55","F50","F45","F40"],
+    realisations=["1","1","1","1","1","1"],
     snapshot=12,
     file_ending="all",
-    labels=["L302-N1136-GR"],
-    colors=["black"],
+    labels=["GR","F60","F55","F50","F45","F40"],
+    colors=["black","blue","magenta","green","orange","red"],
     defaults=[],
-    plot_name="scaling_relation_GR",
+    plot_name="scaling_relation_allModels",
     show_spread=False
 )
 
