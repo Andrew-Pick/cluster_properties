b0ee53d8f08dda0380acd2ad7a29ed5c6a379515
Andrew Pickett <znqg22@durham.ac.uk>
2025-08-05 11:52:41 +0100

    Mass cutoff at 1e13, model F45

--- run_ClusterProperties.py
+++ run_ClusterProperties.py
@@ -2,10 +2,10 @@ from cluster_properties import ClusterProperties
 
 cp = ClusterProperties(
     simulation="L302_N1136",
-    model="F50",
+    model="F45",
     realisation="1",
     snapshot=12,
-    mass_cut=1e11,
+    mass_cut=1e13,
     delta=500,
     file_ending="all",
     rescaling="true",
